c33a3a66ccd1b8e23b730cf85cdecdac1ca6d57b
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-28 16:27:21 +0100

    override method doMeasurement

--- src/main/java/org/example/Aqualung2021ProDivePressureSensorImpl.java
+++ src/main/java/org/example/Aqualung2021ProDivePressureSensorImpl.java
@@ -1,4 +1,11 @@
 package org.example;
 
-public class Aqualung2021ProDivePressureSensorImpl {
+public class Aqualung2021ProDivePressureSensorImpl extends PressureSensor{
+    @Override
+    public void doMeasurement() {
+        double max = 10;
+        double min = 0;
+        double range = max - min;
+        measurementValue = (Math.random() * range) + min;
+    }
 }
