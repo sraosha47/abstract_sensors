606eec52d4282de6a8267e2b1aa77788be8e826f
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-28 15:41:05 +0100

    change datatypes of methods getValue and getUnit

--- src/main/java/org/example/Sensor.java
+++ src/main/java/org/example/Sensor.java
@@ -1,8 +1,8 @@
 package org.example;
 
 abstract class Sensor {
-    public abstract void getUnit();
-    public abstract void getValue();
+    public abstract String getUnit();
+    public abstract double getValue();
     public abstract void getName();
     public abstract void doMeasurement();
 }
