7ba3ef2191176891216436de7515bb37d0b7dc82
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-28 15:39:57 +0100

    extend Sensor, add attributes, override methods

--- src/main/java/org/example/PressureSensor.java
+++ src/main/java/org/example/PressureSensor.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class PressureSensor {
+public abstract class PressureSensor extends Sensor{
+    private String unit;
+    protected double measurementValue;
+    @Override
+    public double getValue(){
+        return measurementValue;
+    }
+    @Override
+    public String getUnit(){
+        return unit;
+    }
 }
+y
