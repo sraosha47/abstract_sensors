a69df6620b651d4eafbc288ed45e071632645b0b
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-28 16:27:07 +0100

    override method doMeasurement

--- src/main/java/org/example/Barometric1000PressureSensorImpl.java
+++ src/main/java/org/example/Barometric1000PressureSensorImpl.java
@@ -1,4 +1,11 @@
 package org.example;
 
-public class Barometric1000PressureSensorImpl {
+public class Barometric1000PressureSensorImpl extends PressureSensor{
+    @Override
+    public void doMeasurement() {
+        double max = 1.05;
+        double min = 0.5;
+        double range = max - min;
+        measurementValue = (Math.random() * range) + min;
+    }
 }
